81c5ac1f60e941233a3d20c8f54ab0d13733dd64
Vamsi <146840998+vamsigajula14@users.noreply.github.com>
2025-01-07 16:11:43 +0000

    port number changed

--- app.py
+++ app.py
@@ -254,6 +254,5 @@ def protected_area():
 
 if __name__ == "__main__":
     create_tables()
-    port = int(os.environ.get("PORT",
-                              8080))  # Default to 8080 if PORT is not set
-    app.run(host='0.0.0.0', port=port)
+
+    app.run(debug=True)
